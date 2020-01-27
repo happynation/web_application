7d28c0c4d82d8b8b9f678d94f163daae08b74481
vovatran1993 <vovatran1993@gmail.com>
2020-01-27 17:48:50 -0600

    added key name

--- instance.tf
+++ instance.tf
@@ -2,6 +2,7 @@ resource "aws_instance" "web" {
   ami           = "ami-062f7200baf2fa504" 
   instance_type = "t2.micro" 
   associate_public_ip_address = "true"
+  key_name = "${aws_key_pair.deployer.key_name}"
  
 
 tags = { 
