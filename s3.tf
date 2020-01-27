4ebd06049fb89b8eb2b3abdf90fa1a055360ad65
vovatran1993 <vovatran1993@gmail.com>
2020-01-27 17:18:57 -0600

    versionong enabled

--- s3.tf
+++ s3.tf
@@ -1,4 +1,13 @@
 resource "aws_s3_bucket" "b" { 
   bucket = "my-tf-test-bucket-vova" 
+  acl    = "private" 
 
+  tags = { 
+    Name        = "My bucket" 
+    Environment = "Dev" 
+  } 
+
+  versioning { 
+    enabled = true 
+  } 
 } 
