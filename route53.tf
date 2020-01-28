58be5701ef0106e62e7149d62c14d278a36c775c
vovatran1993 <vovatran1993@gmail.com>
2020-01-28 00:26:54 -0600

    added rt53

---
+++ route53.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "www" { 
+  zone_id = "Z35QNM44BNN4KR" 
+  name    = "www.rootvovak.com" 
+  type    = "A" 
+  ttl     = "60" 
+  records = ["aws_instance.web.public_ip"] 
+} 
