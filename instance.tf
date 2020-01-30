5ed383bb0c9242531811db023365402ac2b88a1f
vovatran1993 <vovatran1993@gmail.com>
2020-01-29 18:23:43 -0600

    remove provisioner

--- instance.tf
+++ instance.tf
@@ -4,22 +4,9 @@ resource "aws_instance" "web" {
   associate_public_ip_address = "true"
   key_name = "${aws_key_pair.deployer.key_name}"
   security_groups = ["allow_ssh"]
-  provisioner "remote-exec" { 
-    connection { 
-      host        = "${self.public_ip}" 
-      type        = "ssh" 
-      user        = "${var.user}" 
-      private_key = "${file(var.ssh_key_location)}" 
-      } 
-      inline = [ 
-        "sudo yum install -y epel-release",
-        "sudo yum install httpd -y",
-        "systemctl start httpd", 
-        ] 
-        } 
- 
-
-tags = { 
+  
+   
+    tags = { 
     Name = "HelloWorld" 
   } 
 } 
